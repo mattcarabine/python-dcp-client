3d39b25431ab4cf7667a87997c1d5b91a5bb3603
mattcarabine <matt.carabine@couchbase.com>
2016-04-07 13:02:33 +0100

    Add CLI arguments

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
-
+import argparse
 import json
 import threading
 import time
+import sys
 
 from dcp import DcpClient, ResponseHandler
 
@@ -42,23 +43,34 @@ class MyHandler(ResponseHandler):
 def main():
     handler = MyHandler()
     client = DcpClient()
-    host = '10.240.0.3'
-    client.connect(host, 8091, 'bucket1', 'Administrator', 'password',
+    args = parse_arguments(sys.argv[1:])
+    client.connect(args.host, 8091, args.bucket, args.username, args.password,
                    handler)
-    for x in range(1, 10000, 2):
+    while True:
         for i in range(880, 1024):
-            result = client.add_stream(i, host, 0, 0, 1, 0, 0, 0)
+            result = client.add_stream(i, args.host, 0, 0, 1, 0, 0, 0)
             while result['status'] == 2:
                 time.sleep(0.5)
-                result = client.add_stream(i, host, 0, 0, 1, 0, 0, 0)
+                result = client.add_stream(i, args.host, 0, 0, 1, 0, 0, 0)
             if result['status'] != 0:
                 print 'Stream request to vb %d failed due to error %d' % \
                       (i, result['status'])
 
-    while handler.has_active_streams():
-        time.sleep(.25)
-
     client.close()
+
+
+def parse_arguments(program_args):
+    parser = argparse.ArgumentParser(description='DCP client to reproduce MB-19093')
+    parser.add_argument('host', default='localhost',
+                        help='hostname of the couchbase node to test')
+    parser.add_argument('bucket', default='default',
+                        help='name of the bucket to test')
+    parser.add_argument('username', default='Administrator',
+                        help='couchbase username')
+    parser.add_argument('password', default='password',
+                        help='couchbase password')
+    return parser.parse_args(program_args)
+
 
 if __name__ == "__main__":
     main()
